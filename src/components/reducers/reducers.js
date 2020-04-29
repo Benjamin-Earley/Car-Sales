51b77e0e09de10f92420a66237a574af4b5eba31
Benjamin Earley <benjaminearley@MacBook-Pro.local>
2020-04-28 20:50:51 -0400

    Initialized project; Laid foundation for reducers.

---
+++ src/components/reducers/reducers.js
@@ -0,0 +1,49 @@
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: "2019 Ford Mustang",
+      image:
+        "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: "V-6 engine", price: 1700 },
+      { id: 2, name: "Body kit", price: 1200 },
+      { id: 3, name: "22s", price: 1600 },
+      { id: 4, name: "Spoiler", price: 350 }
+    ]
+  };
+  
+  export function reducer(state = initialState, action) {
+    switch (action.type) {
+      case ADD_FEATURES:
+        return {
+          ...state,
+          car: {
+            ...state.car,
+            price: state.car.price + action.payload.price,
+            features: [...state.car.features, action.payload]
+          },
+          additionalFeatures: state.additionalFeatures.filter(
+            item => item.id !== action.payload.id
+          )
+        };
+      case REMOVE_FEATURES:
+        return {
+          ...state,
+          car: {
+            ...state.car,
+            price: state.car.price - action.payload.price,
+            features: state.car.features.filter(
+              item => item.id !== action.payload.id
+            )
+          },
+          additionalFeatures: [...state.additionalFeatures, action.payload]
+        };
+  
+      default:
+        return state;
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import App from './App';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
